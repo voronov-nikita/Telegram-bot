9a26767c438d7521d45d0eb59eea3f012c87d26a
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-03-30 22:52:45 +0300

    update_target

--- scr/Bot-Script.py
+++ scr/Bot-Script.py
@@ -1,17 +1,14 @@
 import sqlite3 as sql
 import requests
 import telebot
-from collecting_data import SendRequestMOS
+from collecting_data import SendRequest
 
 
 list_command = [
-    [],
-    [],
-    []
+    ["!all", "!!al"],
+    ["none"],
+    [str(i) for i in range(1, 10)]
 ]
-
-login = "voronovnr_1"
-password = "V3376863n!"
 
 token_bot = "6089674486:AAEJE6Vajx0gbnQk6bPJjq98J2Dekrpmio0"
 bot = telebot.TeleBot(token_bot, parse_mode="MARKDOWN")
@@ -19,23 +16,31 @@ bot = telebot.TeleBot(token_bot, parse_mode="MARKDOWN")
 
 @bot.message_handler(commands=[f'start'], content_types=["text"])
 def start_function(message):
+    message2 = message
     bot.send_message(message.chat.id, "Привет, прежде чем начать работу, напишите ваше ФИО и класс."
                                       "\nПример: Воронов Никита Рустамович 10А")
 
 
 @bot.message_handler(commands=['help', 'about'], content_types=['text'])
 def write_about_bot(message):
-    with open("text_about_bot.txt", "rb") as text_about_bot:
-        bot.send_message(message.chat.id, text_about_bot)
-        print(message.chat.username)
+    with open("text_about_bot.txt", "rb") as text:
+        bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(content_types=['text'])
 def write_about_bot(message):
     msg = message.text.lower()
 
+    user_id = message.chat.id
+
     if msg in list_command[0]:
-        SendRequestMOS(login=login, password=password).get_homework()
+        bot.send_message(user_id, "OK")
+    elif msg in list_command[1]:
+        bot.send_message(user_id, "KO")
+    elif msg in list_command[2]:
+        bot.send_message(user_id, "LLL")
+    else:
+        bot.send_message(user_id, "Такой команды нет")
 
 
 # запуск бота
--- scr/collecting_data.py
+++ scr/collecting_data.py
@@ -2,17 +2,5 @@ from requests import get
 from bs4 import BeautifulSoup as bs
 
 
-class SendRequestMOS:
-    def __init__(self, login, password):
-        self.login = login
-        self.password = password
-        self.url = "https://school.mos.ru/"
-
-        self.url_homework = "https://dnevnik.mos.ru/diary/homeworks/homeworks"
-
-        self.name_work = None
-        self.url_homework_one = f"https://dnevnik.mos.ru/diary/homeworks/homeworks?subject={self.name_work}"
-
-    def get_homework(self, day=None):
-        get(self.url)
-
+class SendRequest:
+    pass
