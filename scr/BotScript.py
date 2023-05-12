d7d9df8310262b0d0eafd9f1011f282291e8af04
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-05-12 23:35:05 +0300

    bot_can_send_photo_files_and_save_database

--- scr/BotScript.py
+++ scr/BotScript.py
@@ -1,18 +1,14 @@
 import telebot
 from telebot import types
 import sqlite3 as sql
+from PIL import Image
+from io import BytesIO
 from messages import MessageText, ButtonText, AudioLink
 
-
-class Bot():
-    def __init__(self, token):
-        self.create_data_base()
-        self.bot = telebot.TeleBot(token, parse_mode="MARKDOWN")
-
-        self.hello:bool = True
-
-    def create_data_base(self):
-        db = sql.connect("UserInfo.db")
+class DataBaseFunction():
+# =============== Data Base with user information ===============
+    def create_data_base_users(self):
+        db = sql.connect("DataBase.db")
         cursor = db.cursor()
 
         cursor.execute("""CREATE TABLE IF NOT EXISTS users(
@@ -24,7 +20,7 @@ class Bot():
         db.commit()
 
     def add_new_user(self, user_id, name, id_chat):
-        db = sql.connect("UserInfo.db")
+        db = sql.connect("DataBase.db")
         cursor = db.cursor()
         cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
         replay =cursor.fetchone()
@@ -33,9 +29,65 @@ class Bot():
             INSERT INTO users (id, UserName, level) VALUES (?, ?, ?)
             """, (int(user_id), str(name), 1))
             db.commit()
+            db.close()
+            return 1
         else:
-            self.hello = False
-            self.bot.send_message(id_chat, MessageText['replay_start'])
+            db.close()
+            return 0
+
+# ===================== Data Base with photo-file =====================
+    def create_data_base_files(self):
+        db = sql.connect("DataBase.db")
+        cursor = db.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS files(
+            name TEXT,
+            file TEXT
+            )
+        """)
+        db.commit()
+        db.close()
+
+    def add_new_file(self, name, file, bot):
+
+        file_id = file.file_id
+        file_info = bot.get_file(file_id)
+        new_file = bot.download_file(file_info.file_path)
+
+        db = sql.connect("DataBase.db")
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM files WHERE name=?", (name,))
+        replay =cursor.fetchone()
+        if not(replay):
+            cursor.execute("""
+                INSERT INTO files (name, file) VALUES (?, ?)
+                """, (name, new_file))
+            db.commit()
+        db.close()
+
+    def image_from_bytes(self, user_id, file, bot):
+        stream = BytesIO(file)
+        image = Image.open(stream)
+        bot.send_photo(user_id, image)
+
+    def get_from_database(self, user_id, bot, name_file):
+        db = sql.connect("DataBase.db")
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM files WHERE name=?", (name_file, ))
+
+        rows = cursor.fetchall()
+        self.image_from_bytes(user_id, rows[0][1], bot)
+
+        db.close()
+
+
+class Bot():
+    def __init__(self, token):
+        self.bd = DataBaseFunction()
+
+        self.bd.create_data_base_users()
+        self.bd.create_data_base_files()
+        self.bot = telebot.TeleBot(token, parse_mode="MARKDOWN")
+
 
     def run(self):
         # <-------------- Commands for bot -------------->
@@ -43,15 +95,18 @@ class Bot():
         def start_message(message):
             user_id = message.from_user.id
             user_name = message.from_user.username
-            self.add_new_user(user_id, user_name, message.chat.id)
-            
-            if self.hello:
+
+            res = self.bd.add_new_user(user_id, user_name, message.chat.id)
+
+            if not(res):
                 self.bot.send_message(message.chat.id, MessageText['replay_start'])
             else:
+            
+                self.bot.send_message(message.chat.id, MessageText['hello'])
                 keyboard = types.ReplyKeyboardMarkup(row_width=2)
 
                 # Создаем кнопки
-                button1 = types.KeyboardButton('/find')
+                button1 = types.KeyboardButton('/image')
                 button2 = types.KeyboardButton('/music')
                 button3 = types.KeyboardButton('/help')
                 button4 = types.KeyboardButton('/more')
@@ -68,7 +123,11 @@ class Bot():
 
         @self.bot.message_handler(commands=["music"], content_types=["text"])
         def music_message(message):
-            self.bot.send_audio(message.chat.id, AudioLink['first'])
+            self.bot.send_audio(message.chat.id, audio=AudioLink['first'])
+
+        @self.bot.message_handler(commands=["image"], content_types=["text"])
+        def photo_message(message):
+            self.bd.get_from_database(message.chat.id, name_file="code", bot=self.bot)
 
         @self.bot.message_handler(commands=["more"], content_types=["text"])
         def more_message(message):
@@ -91,6 +150,11 @@ class Bot():
         @self.bot.message_handler(func=lambda message: True)
         def listen_message(message):
             self.bot.send_message(message.chat.id, message.text)
+
+        @self.bot.message_handler(content_types=['photo'])
+        def get_image(message):
+            self.bd.add_new_file(name="code", file=message.photo[-1], bot=self.bot)
+            self.bot.send_message(message.chat.id, MessageText['successful'])
 
         @self.bot.callback_query_handler(func=lambda call: True)
         def listen_button_call(call):
--- scr/test.py
+++ scr/test.py
@@ -1,37 +1,38 @@
 import telebot
-from telebot import types
+from PIL import Image
+import io
 
 bot_token = "6089674486:AAEJE6Vajx0gbnQk6bPJjq98J2Dekrpmio0"
 
+def from_bytes(user_id, file):
+    stream = io.BytesIO(file)
+
+    # Используем метод open из модуля Image, чтобы открыть изображение из потока данных
+    image = Image.open(stream)
+
+    # Теперь вы можете использовать объект image для выполнения дополнительной обработки изображения
+    # Например, можно сохранить изображение на диск:
+    bot.send_photo(user_id, image)
+
 bot = telebot.TeleBot(bot_token)
+ls = []
+# Обработка сообщений с изображениями
+@bot.message_handler(content_types=['photo'])
+def handle_image(message):
+    photo = message.photo[-1]  # Берем последнее изображение из списка
+    file_id = photo.file_id
+    file_info = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
 
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    # Создаем объект клавиатуры
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    byte_array = bytearray(downloaded_file)
+    ls.append(byte_array)
 
-    # Создаем кнопки
-    button1 = types.InlineKeyboardButton('Кнопка 1', callback_data='button1', switch_inline_query_current_chat='')
-    button2 = types.InlineKeyboardButton('Кнопка 2', callback_data='button2', switch_inline_query_current_chat='')
-    button3 = types.InlineKeyboardButton('Кнопка 3', callback_data='button3', switch_inline_query_current_chat='')
-    button4 = types.InlineKeyboardButton('Кнопка 4', callback_data='button4', switch_inline_query_current_chat='')
+    # Дальнейшая обработка массива байтов (byte_array)
+    # Например, сохранение в базу данных или дальнейшая обработка изображения
+    
+    # Ответ пользователю
+    bot.reply_to(message, 'Изображение принято и обработано')
+    from_bytes(message.chat.id, ls[0])
 
-    # Добавляем кнопки на клавиатуру
-    keyboard.add(button1, button2)
-    keyboard.add(button3, button4)
-
-    # Отправляем сообщение с клавиатурой
-    bot.send_message(message.chat.id, 'Выберите опцию:', reply_markup=keyboard)
-
-@bot.callback_query_handler(func=lambda call: True)
-def handle_callback(call):
-    if call.data == 'button1':
-        bot.send_message(call.message.chat.id, 'Вы нажали кнопку 1')
-    elif call.data == 'button2':
-        bot.send_message(call.message.chat.id, 'Вы нажали кнопку 2')
-    elif call.data == 'button3':
-        bot.send_message(call.message.chat.id, 'Вы нажали кнопку 3')
-    elif call.data == 'button4':
-        bot.send_message(call.message.chat.id, 'Вы нажали кнопку 4')
-
+# Запуск бота
 bot.polling()
